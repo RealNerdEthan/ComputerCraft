813956b9dd47d5e525a5338ba2002f7f1fd5dcb4
Ethan Goddard <38088906+RealNerdEthan@users.noreply.github.com>
2020-02-23 21:37:56 -0500

    Create portable_base.lua

---
+++ portable_base.lua
@@ -0,0 +1,35 @@
+--[[Pastebin URL:https://pastebin.com/dbJKzPyE]]--
+sleep(2)
+
+--[[Base Row 1]]--
+turtle.select(1)
+turtle.place
+turtle.forward
+turtle.place
+turtle.forward
+turtle.place
+turtle.forward
+turtle.place
+turtle.forward
+turtle.place
+turtle.turnLeft
+turtle.place
+turtle.forward
+turtle.turnLeft
+
+--[[Base Row 2]]--
+turtle.select(2)
+turtle.place
+turtle.forward
+turtle.place
+turtle.forward
+turtle.place
+turtle.forward
+turtle.select(1)
+turtle.place
+turtle.forward
+turtle.turnRight
+turtle.select(1)
+turtle.place
+turtle.forward
+turtle.turnRight
